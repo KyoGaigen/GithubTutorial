4376503b3afb6aa91300c2b435253582dce09048
Jesse010743 <jessejiang010@gmail.com>
2023-05-17 14:31:33 +0900

    Initial Commit

---
+++ GithubTutorial/GithubTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTutorialApp.swift
+//  GithubTutorial
+//
+//  Created by Kaiyuan Jiang on 2023/05/17.
+//
+
+import SwiftUI
+
+@main
+struct GithubTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
